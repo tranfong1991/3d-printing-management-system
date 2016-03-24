53f96a9561ed71c6f77b93d102c02c8b924f373c
Jeremy Moorman <jsmoorman@tamu.edu>
2016-03-24 09:33:03 -0500

    Added bootstrap CSS/JS and POST method for upload (still no logic though)

--- webapp/app/controllers/print_controller.rb
+++ webapp/app/controllers/print_controller.rb
@@ -1,7 +1,14 @@
 class PrintController < ApplicationController
 
+  def new
+
+  end
+
   def upload
-    
+    # TODO: Logic for checking if student exists in database
+    flash[:success] = "You are authorized to print" # If student is in database
+    # flash[:danger] = "You are not authorized to print" # If student is NOT in database
+    redirect_to prints_path
   end
 
 end
