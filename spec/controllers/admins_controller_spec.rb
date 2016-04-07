a2908ca3dda71701bd4f89acec81e713bb006425
santos22 <santosjavier22@gmail.com>
2016-04-06 23:00:59 -0500

    Wrote more tests but less coverage - need to resolve admin login info to complete tests

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe AdminsController, :type => :controller do
   describe 'templates' do
+    before (:each) do
+      session[:username] = "troll"
+      session[:password_digest] = "lol"
+    end
+
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
@@ -10,6 +15,19 @@ describe AdminsController, :type => :controller do
     it "renders the new template" do
       { :get => new_student_path(1) }.
         should route_to(:controller => "students", :action => "new", :format => "1")
+    end
+
+    it "should create new admin" do
+      Admin.stub(:new).and_return(double(Admin))
+      post :new
+      #flash[:notice].should =~ /successfully created/i
+    end
+
+    it "should go to signup page" do
+      {:get => signup_path}.should route_to(
+        :controller => "admins",
+        :action => "new"
+      )
     end
   end
 end
