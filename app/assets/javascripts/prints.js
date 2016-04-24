68bcab38117a9d23f957f94761edc0ab301d2db6
Andy Tran <tran_fong1991@yahoo.com>
2016-04-24 20:00:08 +0000

    fix filename when upload

--- app/assets/javascripts/prints.js
+++ app/assets/javascripts/prints.js
@@ -1,21 +1,31 @@
 function update_status(id, status) {
   $.post("/prints/update_status.js", {id: id, status: status}, function(){
     document.getElementById('status_' + id).innerHTML = status;
-    document.getElementById('status_btn_' + id).innerHTML=""
+    document.getElementById('status_btn_' + id).innerHTML="";
     if(status == 'started'){
-      document.getElementById('status_btn_' + id).innerHTML="<button id=\"finish_btn_" + id + "\" onclick=\"update_status(" + id + ", 'completed')\">Complete</button><button id=\"abort_btn_" + id + "\" onclick=\"update_status(" + id + ", 'aborted')\">Abort</button>"
+      document.getElementById('status_btn_' + id).innerHTML="<button id=\"finish_btn_" + id + "\" onclick=\"update_status(" + id + ", 'completed')\">Complete</button><button id=\"abort_btn_" + id + "\" onclick=\"update_status(" + id + ", 'aborted')\">Abort</button>";
       window.alert("Print started!");
     }
     else if(status == 'completed'){
-      document.getElementById('print_' + id).remove()
+      document.getElementById('print_' + id).remove();
       window.alert("Print marked as complete!");
     }
     else if(status == 'aborted'){
-      document.getElementById('status_btn_' + id).innerHTML="<button id=\"restart_btn_" + id + "\" onclick=\"update_status(" + id + ", 'started')\">Restart</button><button id=\"cancel_btn_" + id + "\" onclick=\"update_status(" + id + ", 'canceled')\">Cancel</button>"
+      document.getElementById('status_btn_' + id).innerHTML="<button id=\"restart_btn_" + id + "\" onclick=\"update_status(" + id + ", 'started')\">Restart</button><button id=\"cancel_btn_" + id + "\" onclick=\"update_status(" + id + ", 'canceled')\">Cancel</button>";
       window.alert("Print aborted...");
     }
   })
   .fail(function(){
-    window.alert("Cannot update status...")
-  })
+    window.alert("Cannot update status...");
+  });
+}
+
+function cancel_print(id, status){
+   $.post("/prints/update_status.js", {id: id, status: status}, function(){
+      document.getElementById('cancel_btn').style.visibility = 'hidden';
+      document.getElementById('status').innerHTML = status;
+   })
+   .fail(function(){
+     
+   });
 }
