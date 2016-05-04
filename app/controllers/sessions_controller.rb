641cc24da0ad63d450abb97f61cd576d61f9ee55
Andy Tran <tran_fong1991@yahoo.com>
2016-05-04 00:18:12 +0000

    working ENV variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,8 @@ class SessionsController < ApplicationController
         @admin = Admin.find_by(:username => params[:session][:username])
         if @admin && @admin.authenticate(params[:session][:password])
             session[:admin_id] = @admin.id
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 8251c2f7663bf62a5d40baea9c65cc69c7c6edc7
             flash[:success] = "Successfully signed in"
             redirect_to '/'
-=======
-            redirect_to '/prints/new'
->>>>>>> ca2fbb25dd7abe220f1a6405e36910bf4fe41870
         else
             flash[:danger] = "Incorrect username or password! Please try again."
             redirect_to '/login'
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
 require 'aws-sdk'
 
 Aws.config.update({
-                      region: 'us-west-2',
-                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                      ssl_verify_peer: false
-                  })
+  region: 'us-west-2',
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  ssl_verify_peer: false
+})
